b921ab1d0d2c92efd673f644af24ac1b2935c0f9
Gideon Govind <gideon.govind@rubicotech.in>
2022-08-29 11:26:07 +0530

    fix: ebay product price capture issue

--- plugins/domains/ebay.com.js
+++ plugins/domains/ebay.com.js
@@ -2,10 +2,10 @@ import log from '../../logging.js';
 export { log };
   
 export default {
-  re: [
-    /^(https):\/\/[www.]{0,}ebay.com\/itm\/[0-9]{5,}/i,
-    /^(https):\/\/[www.]{0,}ebay.com\/p\/[0-9]{5,}/i
-  ],
+  // re: [
+  //   /^(https):\/\/[www.]{0,}ebay.com\/itm\/[0-9]{5,}/,
+  //   /^(https):\/\/[www.]{0,}ebay.com\/p\/[0-9]{5,}/i
+  // ],
 
   lowestPriority: true,
 
@@ -17,9 +17,9 @@ export default {
     var $priceScope = cheerio('[itemprop="price"]');
     var price = $priceScope.attr('content');
     if (typeof price !== 'undefined') {
-      return {
+      return cb(null, {
         price: price,
-      }
+      });
     } else {
       request({
         uri: url,
